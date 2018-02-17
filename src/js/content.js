5d3fb5568e03c7f71ca6297f858d67db34f9050d
John McIntosh <johntmcintosh@gmail.com>
2018-02-16 22:48:42 -0600

    Add URI component decoding to the file identifier to better support filenames with special characters

--- src/js/content.js
+++ src/js/content.js
@@ -304,7 +304,7 @@
 					_$diffSections.hide();
 
 					// Show the selected section
-					var sectionId = fileIdentifier.replace('#', '').replace(/%20/g, ' ');
+					var sectionId = decodeURIComponent(fileIdentifier.replace('#', ''));
 					var $section = $('section[id*="' + sectionId + '"]');
 					$section.show();
 
